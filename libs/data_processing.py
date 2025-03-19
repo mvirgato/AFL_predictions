Made using the model more streamlined. Not just run the script predictors.py in libs and input round when prompted. Also fixed bug where softmax was being called in training. crossentropy loss needs raw logits!

libs/data_processing.py
@@ -6,7 +6,7 @@ from glob import glob
 import re
 import os
 
-years = np.arange(1970, 2025)
+years = np.arange(1970, 2026)
 rounds = np.arange(1, 29)
 
 headers = {'User-Agent':'MV_tipping_predictions'}
@@ -35,6 +35,8 @@ def read_ranking(file):
 
     _df.insert(loc=0, column='round', value=_round * np.ones(len(_df), dtype= int))
 
+    _df = _df.dropna()
+
     return _df
 
 
@@ -44,6 +46,7 @@ def get_team_info():
     Returns a dictionary to translate team name to ID number for use 
     in neural nets
     '''
+
     if not isfile('team_data.csv'):
         team_data = requests.get('https://api.squiggle.com.au/?q=teams;format=csv', headers=headers)
 
@@ -61,9 +64,14 @@ def get_team_info():
 
 def get_seasonal_data():
 
+    try:
+        os.mkdir('Seasonal_Data')
+    except:
+        pass
+
     for year in years:
 
-        if isfile(f'Seasonal_Data/data_{year:d}.csv'):
+        if isfile(f'Seasonal_Data/data_{year:d}.csv') and year != 2025:
 
             # print('Data already downloaded.')
             
@@ -74,15 +82,22 @@ def get_seasonal_data():
         with open(f'Seasonal_Data/data_{year:d}.csv', 'w') as f:
             f.write(response.text)
 
-    rounds = np.arange(1, 29)
+        if year == 2025:
+            _df = pd.read_csv(f'Seasonal_Data/data_{year:d}.csv')
+            _df['round'] += 1
+
+            _df.to_csv(f'Seasonal_Data/data_{year:d}.csv', index=False)
+
 
 def get_standings():
 
     for year in years:
+
         try:
             os.mkdir(f'Standings/{year:d}/')
         except:
             pass
+
         for round_ in rounds:
 
             if isfile(f'Standings/{year:d}/round_{round_:d}.csv'):
@@ -135,10 +150,11 @@ def process_season_data(datafile):
 
     dataframe = pd.read_csv(datafile)
 
+    dataframe = dataframe.dropna(subset=['agoals'])
+
     dataframe = dataframe[['round', 'hteamid', 'ateamid', 'hscore', 
                            'ascore', 'hgoals', 'agoals', 'hbehinds', 
                            'abehinds', 'is_final', 'winnerteamid']].copy()
-
     # Fill NaN values in winnerteamid
     dataframe['winnerteamid'] = dataframe['winnerteamid'].fillna(0)
 
@@ -184,13 +200,14 @@ def process_season_data(datafile):
 def process_data():
 
     data_files = glob('Seasonal_Data/data_*.csv')
+    data_files = sorted(data_files)
     full_data = pd.concat(process_season_data(file) for file in data_files)
     return full_data
 
 
 def current_round_data(_round):
 
-    current_round = requests.get('https://api.squiggle.com.au/?q=games;year=2025;round={_round:d}};format=csv', headers=headers)
+    current_round = requests.get(f'https://api.squiggle.com.au/?q=games;year=2025;round={_round:d};format=csv', headers=headers)
 
     with open('current_round.csv', 'w') as f:
         f.write(current_round.text)

libs/neural_nets.py
@@ -25,29 +25,47 @@ class PandasDataset(Dataset):
 
 # Define architecture for the dense neural network
 class DenseNN(nn.Module):
-    def __init__(self,inputNum, hidden_nodes, output_nodes):
-
+    def __init__(self, inputNum, hidden_nodes, output_nodes, num_hidden_layers):
         super(DenseNN, self).__init__()
         
-        self.inputNum=inputNum
+        self.inputNum = inputNum
         
-        self.fc1 = nn.Linear(in_features=inputNum, out_features=hidden_nodes)  # Input layer
-        self.fc2 = nn.Linear(in_features=hidden_nodes, out_features=hidden_nodes)     # Hidden layer
-        self.fc3 = nn.Linear(in_features=hidden_nodes, out_features=hidden_nodes)     # Hidden layer
-        self.fc4 = nn.Linear(in_features=hidden_nodes, out_features=hidden_nodes)     # Hidden layer
-        # self.fc5 = nn.Linear(in_features=hidden_nodes, out_features=hidden_nodes)     # Hidden layer
-        self.fc5 = nn.Linear(in_features=hidden_nodes, out_features=output_nodes)     # Output layer
+        # Input layer
+        self.input_layer = nn.Linear(in_features=inputNum, out_features=hidden_nodes)
+        
+        # Dynamically create hidden layers
+        self.hidden_layers = nn.ModuleList([
+            nn.Linear(in_features=hidden_nodes, out_features=hidden_nodes) 
+            for _ in range(num_hidden_layers)
+        ])
+        
+        # Output layer
+        self.output_layer = nn.Linear(in_features=hidden_nodes, out_features=output_nodes)
 
     def forward(self, x):
+        x = x.view(-1, self.inputNum)  # Flatten the input
         
-        x = x.view(-1, self.inputNum)   # Flatten the input
+        # Pass through input layer
+        x = torch.relu(self.input_layer(x))
         
-        x = torch.relu(self.fc1(x))
-        x = torch.relu(self.fc2(x))
-        x = torch.relu(self.fc3(x))
-        x = torch.relu(self.fc4(x))
-        # x = torch.relu(self.fc5(x))
-        x = self.fc5(x)
+        # Pass through hidden layers
+        for layer in self.hidden_layers:
+            x = torch.relu(layer(x))
+        
+        # Pass through output layer
+        x = self.output_layer(x)
+        
+        # return F.softmax(x, dim=1)
+        return x
+    
+    def predict(self, x):
+        """
+        Perform inference and return probabilities using softmax.
+        """
+        self.eval()  # Set the model to evaluation mode
+        
+        with torch.no_grad():  # Disable gradient computation for inference
+            logits = self.forward(x)  # Get raw logits
+            probabilities = F.softmax(logits, dim=1)  # Apply softmax to get probabilities
 
-        return F.softmax(x, dim=1)
-        # return x
+        return probabilities